ecf115a82058140daf13fd39cfc482d60db03b7c
Arijit Sengupta <arijitsengupta1976@gmail.com>
2025-01-22 12:50:28 +0530

    Integrated user and captain endpoints with react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,11 @@ import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
 
 
 
@@ -19,7 +24,27 @@ const App=()=>{
         <Route path = '/signup' element={<UserSignup />} />
         <Route path = '/captain-login' element={<CaptainLogin />} />
         <Route path = '/captain-signup' element={<CaptainSignup />} />
-        <Route path = '/home' element={<Home />} />
+        <Route path = '/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
+        <Route path = '/user/logout' element={
+          <UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+        } />
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+            <CaptainHome />
+          </CaptainProtectWrapper>
+
+        } />
+        <Route path='/captain/logout' element={
+          <CaptainProtectWrapper>
+            <CaptainLogout />
+          </CaptainProtectWrapper>
+        } />
 
       </Routes>
     </div>
